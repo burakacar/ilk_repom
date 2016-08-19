f71b8538c4b695b6a4a427ba112f433a2bee183f
burakacar <acarburak@itu.edu.tr>
2016-08-19 14:35:35 +0300

    Add files via upload

---
+++ burak.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(void)
+{
+ printf("Hello sana dunya\n");
+ int toplam;
+ toplam = topla(3,4);
+ printf("%d",toplam);
+
+}
+int topla(int a,int b){
+  int result=a+b;
+  return result;
+}
